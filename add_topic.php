5917b69dfa3fc462b89126635967cc3bf2d09726
wallenium <wallenium@eqdkp-plus.eu>
2014-12-13 12:27:23 +0000

    - NO CHANGES - file headers updated

--- add_topic.php
+++ add_topic.php
@@ -1,19 +1,22 @@
 <?php
- /*
- * Project:		EQdkp-Plus
- * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:		2008
- * Date:		$Date: 2012-11-24 18:48:21 +0100 (Sa, 24. Nov 2012) $
- * -----------------------------------------------------------------------
- * @author		$Author: hoofy_leon $
- * @copyright	2006-2011 EQdkp-Plus Developer Team
- * @link		http://eqdkp-plus.com
- * @package		eqdkp-plus
- * @version		$Rev: 12504 $
- * 
- * $Id: add_topic.php 12504 2012-11-24 17:48:21Z hoofy_leon $
+/*	Project:	EQdkp-Plus
+ *	Package:	Officer conference Portal Module
+ *	Link:		http://eqdkp-plus.eu
+ *
+ *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 define('EQDKP_INC', true);
