d00fa63a7bbdac8a0fbe84c7510862ae0e4285f3
godmod <godmod@eqdkp-plus.eu>
2014-12-17 20:34:39 +0000

    Added languages

--- language/english.php
+++ language/english.php
@@ -1,6 +1,6 @@
 <?php
 /*	Project:	EQdkp-Plus
- *	Package:	Officer conference Portal Module
+ *	Package:	EQdkp-Plus Language File
  *	Link:		http://eqdkp-plus.eu
  *
  *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
@@ -19,55 +19,59 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if ( !defined('EQDKP_INC') ){
-	header('HTTP/1.0 404 Not Found');exit;
+ 
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$lang = array(
-	'offi_conf'				=> 'Officer Conference - Topics',
-	'offi_conf_name'		=> 'Officer Conference Module',
-	'offi_conf_desc'		=> 'Admins can post topics for officer conference.',
+//Language: English	
+//Created by EQdkp Plus Translation Tool on  2014-12-17 21:28
+//File: portal/offi_conf/language/english.php
+//Source-Language: german
 
-	//settings
-	'oc_f_day'				=> 'Day of Officer Conference',
-	'oc_f_end_time'			=> 'wanted end of officer conference (HH:MM)',
-	'oc_f_start_time'		=> 'wanted begin of officer conference (HH:MM)',
-	'oc_f_time_type'		=> 'Define start or end of officer conference?',
-	'oc_f_date'				=> 'Date of the next officer meeting',
-	'oc_f_period2'			=> 'Officer meeting every x weeks',
-	'oc_f_period3'			=> 'Officer meeting every x weekday of the month',
-	'oc_f_type'				=> 'Repeat-type',
-	'oc_weekday'			=> 'on a certain weekday',
-	'oc_xmonthday'			=> 'on the x. wekday of the month',
-	'oc_manual'				=> 'manual date entry',
-	'oc_monday'				=> 'Monday',
-	'oc_tuesday'			=> 'Tuesday',
-	'oc_wednesday'			=> 'Wednesday',
-	'oc_thursday'			=> 'Thursday',
-	'oc_friday'				=> 'Friday',
-	'oc_saturday'			=> 'Saturday',
-	'oc_sunday'				=> 'Sunday',
+$lang = array( 
+	"offi_conf" => 'Officer Conference - Topics',
+	"offi_conf_name" => 'Officer Conference Module',
+	"offi_conf_desc" => 'Admins can post topics for officer conference.',
+	"oc_f_day" => 'Day of Officer Conference',
+	"oc_f_end_time" => 'wanted end of officer conference (HH:MM)',
+	"oc_f_start_time" => 'wanted begin of officer conference (HH:MM)',
+	"oc_f_time_type" => 'Define start or end of officer conference?',
+	"oc_f_date" => 'Date of the next officer meeting',
+	"oc_f_period2" => 'Officer meeting every x weeks',
+	"oc_f_period3" => 'Officer meeting every x weekday of the month',
+	"oc_f_type" => 'Repeat-type',
+	"oc_weekday" => 'on a certain weekday',
+	"oc_xmonthday" => 'on the x. wekday of the month',
+	"oc_manual" => 'manual date entry',
+	"oc_monday" => 'Monday',
+	"oc_tuesday" => 'Tuesday',
+	"oc_wednesday" => 'Wednesday',
+	"oc_thursday" => 'Thursday',
+	"oc_friday" => 'Friday',
+	"oc_saturday" => 'Saturday',
+	"oc_sunday" => 'Sunday',
+	"oc_add_topic" => 'Add Topic',
+	"oc_upd_topic" => 'Edit Topic',
+	"oc_next_topics" => 'Next Topics',
+	"oc_topic_name" => 'Name',
+	"oc_topic_time" => 'Time to talk (min)',
+	"oc_topic_posi" => 'Sorting',
+	"oc_topic_desc" => 'Description',
+	"oc_save" => 'Save',
+	"oc_delete" => 'Delete',
+	"oc_save_success" => 'Saving successful!',
+	"oc_save_no_success" => 'Saving not successful!',
+	"oc_delete_success" => 'Deletion successful!',
+	"oc_delete_no_success" => 'Deletion not successful!',
+	"oc_no_name" => 'No Name',
+	"oc_no_desc" => 'No Description',
+	"oc_creator" => 'created by:',
+	"oc_min" => 'min',
+	"oc_next_oc" => 'Next Officer Conference',
+	"oc_start" => 'Begin',
+	"oc_end" => 'End',
 	
-	//add topic
-	'oc_add_topic'			=> 'Add Topic',
-	'oc_upd_topic'			=> 'Edit Topic',
-	'oc_next_topics'		=> 'Next Topics',
-	'oc_topic_name'			=> 'Name',
-	'oc_topic_time'			=> 'Time to talk (min)',
-	'oc_topic_posi'			=> 'Sorting',
-	'oc_topic_desc'			=> 'Description',
-	'oc_save'				=> 'Save',
-	'oc_delete'				=> 'Delete',
-	'oc_save_success'		=> 'Saving successful!',
-	'oc_save_no_success'	=> 'Saving not successful!',
-	'oc_delete_success'		=> 'Deletion successful!',
-	'oc_delete_no_success'	=> 'Deletion not successful!',
-	'oc_no_name'			=> 'No Name',
-	'oc_no_desc'			=> 'No Description',
-	'oc_creator'			=> 'created by:',
-	'oc_min'				=> 'min',
-	'oc_next_oc'			=> 'Next Officer Conference',
-	'oc_start'				=> 'Begin',
-	'oc_end'				=> 'End',
 );
+
 ?>
